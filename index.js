f5bcde7bb2c897fc6088588347caa79af4b195a3
william <wwwy3y3@gmail.com>
2015-10-22 22:51:57 +0800

    ok

--- index.js
+++ index.js
@@ -1,77 +1,10 @@
-var EOL = require('os').EOL
-var lineReader = require('line-reader')
-var semver = /\[?v?([\w\d\.-]+\.[\w\d\.-]+[a-zA-Z0-9])\]?/
-var log = { versions: [] }
-var current
+var Parser= require('./lib/parser');
 
-function parseChangelog (file, callback) {
-  lineReader.eachLine(file, handleLine, EOL).then(function () {
-    // push last version into log
-    pushCurrent()
+// parser
+exports.Parser= Parser;
 
-    // clean up description
-    log.description = clean(log.description)
-    if (log.description === '') delete log.description
-
-    callback(null, log)
-  })
+// parse string
+exports.parseString= function (content) {
+  var parser= new Parser('string');
+  return parser.parse(content);
 }
-
-function handleLine (line) {
-  // skip line if it's a link label
-  if (line.match(/^\[[^\[\]]*\] *?:/)) return
-
-  // set title if it's there
-  if (!log.title && line.match(/^# ?[^#]/)) {
-    log.title = line.substring(1).trim()
-    return
-  }
-
-  // new version found!
-  if (line.match(/^## ?[^#]/)) {
-    if (current && current.title) pushCurrent()
-
-    current = versionFactory()
-
-    if (semver.exec(line)) current.version = semver.exec(line)[1]
-
-    current.title = line.substring(2).trim()
-
-    return
-  }
-
-  // deal with body or description content
-  if (current) {
-    current.body += line + EOL
-  } else {
-    log.description = (log.description || '') + line + EOL
-  }
-}
-
-function versionFactory () {
-  return {
-    version: null,
-    title: null,
-    body: ''
-  }
-}
-
-function pushCurrent () {
-  current.body = clean(current.body)
-  log.versions.push(current)
-}
-
-function clean (str) {
-  if (!str) return ''
-
-  // trim
-  str = str.trim()
-  // remove leading newlines
-  str = str.replace(new RegExp('[' + EOL + ']*'), '')
-  // remove trailing newlines
-  str = str.replace(new RegExp('[' + EOL + ']*$'), '')
-
-  return str
-}
-
-module.exports = parseChangelog
--- test/index.js
+++ test/index.js
@@ -1,42 +1,7 @@
-var parseChangelog = require('..')
-var test = require('tape')
-var EOL = require('os').EOL
-
-var expected = {
-  title: 'changelog title',
-  description: 'A cool description (optional).',
-  versions: [
-    { version: null,
-      title: 'unreleased',
-      body: '* foo' },
-    { version: 'x.y.z',
-      title: 'x.y.z - YYYY-MM-DD',
-      body: '* bar' },
-    { version: 'a.b.c',
-      title: '[a.b.c]',
-      body: '### Changes' + EOL + EOL + '* Update API' + EOL + '* Fix bug #1' },
-    { version: '2.2.3-pre.1',
-      title: '2.2.3-pre.1 - 2013-02-14',
-      body: '* Update API' },
-    { version: '2.0.0-x.7.z.92',
-      title: '2.0.0-x.7.z.92 - 2013-02-14',
-      body: '* bark bark' + EOL + '* woof' + EOL + '* arf' },
-    { version: '1.3.0',
-      title: 'v1.3.0',
-      body: '* make it so' },
-    { version: '1.2.3',
-      title: '[1.2.3](link)',
-      body: '* init' }
-  ]
-}
-
-test('meet expectations', function (t) {
-  t.plan(1)
-
-  parseChangelog(__dirname + '/CHANGELOG.md', function (err, result) {
-    if (err) throw err
-
-    t.deepEqual(result, expected)
-    t.end()
-  })
+var changeLogParser= require('../');
+var path= require('path');
+var md= require('fs').readFileSync(path.join(__dirname, 'changelog2.md'), 'utf8');
+changeLogParser.parseString(md)
+.then(function (logs) {
+	console.log(logs)
 })
